97c1801cd0aa20056ea1c22c7939f54a05f42303
GBTC440017UR <craighunt210@gmail.com>
2019-04-10 21:27:23 -0400

    Initial commit

---
+++ src/main/java/com/cwhcode/bullhorn1/Bullhorn1Application.java
@@ -0,0 +1,13 @@
+package com.cwhcode.bullhorn1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bullhorn1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bullhorn1Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/cwhcode/bullhorn1/Message.java
@@ -0,0 +1,62 @@
+package com.cwhcode.bullhorn1;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Entity
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    String content;
+
+    @NotNull
+    String sentby;
+
+    //Date posteddate =  new Date();
+
+    String posteddate;
+
+    public Message() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSentby() {
+        return sentby;
+    }
+
+    public void setSentby(String sentby) {
+        this.sentby = sentby;
+    }
+
+    public Date getPosteddate() {
+        return posteddate;
+    }
+
+    public void setPosteddate(Date posteddate) {
+        this.posteddate = posteddate;
+    }
+}
